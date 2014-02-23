ab19cfca856f13c83d916e8b17af61811171b956
Chris Jacobs <chris@chrisjacobs.com>
2014-02-23 03:42:36 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        
+        <title>PHP Template</title>
+        
+        
+        
+       
+    </body>
+</html>
